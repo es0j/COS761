c8cd404f3fe11bed3f3ccdefbf1abb7ad312f3ac
Jos3Luiz <zekkarock11@poli.ufrj.br>
2024-03-31 00:52:54 -0300

    Part 1, web fingerprint

--- warmup.js
+++ warmup.js
@@ -23,7 +23,25 @@ function measureNLines() {
 
   // TODO: Exercise 1-1
 
+  const totalMeasures=100000; // Nao tem como garantir miss, mas deve ser o suficiente
+  const LINE_SIZE = 16; // 64/sizeof(int)
+  const M = new Array(totalMeasures * LINE_SIZE).fill(-1);
 
+  //fazer 10 medicoes de 1 - 10kk
+  for (let i = 1; i < totalMeasures; i=i*10) {
+    const start = performance.now();
+    for (let j=0 ; j < i; j++){
+        let val = M[ (i * LINE_SIZE) % M.length];
+    }
+    
+    const end = performance.now();
+    result.push(end - start);
+  }
+
+  /*
+  1 Cache Line: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  N Cache Lines: [0, 0, 0, 0, 0.20000000018626451, 0.7000000001862645, 1.5]
+ */
   return result;
 }
 
